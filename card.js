86b04e90a168ce5ecc204e69ffad894312379a44
djwilliams2020 <68558050+djwilliams2020@users.noreply.github.com>
2020-09-07 14:53:20 -0600

    my first commit

---
+++ card.js
@@ -0,0 +1,34 @@
+
+var noOfGuess = 10;
+var pass = 5;
+var guessChoices = ['star', 'circle', 'square', 'wave', 'plus'];
+var rightAns = 0;
+var winAudio = new Audio('sound/win.wav');
+var mainCard = document.getElementById('maincard');
+var choices = document.getElementsByClassName('choice');
+
+for (i = 0; i < choices.length; i++) {
+    let choice = choices[i];
+    choice.addEventListener('click', function () {
+        let randomIndex = Math.floor((Math.random() * guessChoices.length));
+        let current = guessChoices[randomIndex];
+        console.log(current);
+        console.log(this.id);
+        mainCard.src = 'images/' + current + '.svg';
+        if (current == this.id) {
+            rightAns++;
+            winAudio.play();
+        } else {
+            window.navigator.vibrate(200);
+        }
+        noOfGuess--;
+        if (noOfGuess == 0) {
+            mainCard.src = 'images/back.svg';
+            if (rightAns >= pass) {
+                alert('Congratulations!');
+            } else {
+                alert('Try Again!');
+            }
+        }
+    });
+}
