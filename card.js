2119163d9bfdef7ea9069fa7b64a9abb056699cf
djwilliams2020 <68558050+djwilliams2020@users.noreply.github.com>
2020-10-06 11:26:32 -0600

    required

--- card.js
+++ card.js
@@ -6,29 +6,40 @@ var rightAns = 0;
 var winAudio = new Audio('sound/win.wav');
 var mainCard = document.getElementById('maincard');
 var choices = document.getElementsByClassName('choice');
+var msg = 'Congratulations you have earned ESP!';
+var score = 0;
 
 for (i = 0; i < choices.length; i++) {
     let choice = choices[i];
     choice.addEventListener('click', function () {
         let randomIndex = Math.floor((Math.random() * guessChoices.length));
+        console.log('the randome index is', randomIndex)
         let current = guessChoices[randomIndex];
-        console.log(current);
+        console.log('this is the guess choive at index', guessChoices)
+        console.log('this is current', current);
         console.log(this.id);
         mainCard.src = 'images/' + current + '.svg';
+        console.log('show me the main card', mainCard.src)
+        console.log('this is this.id', this.id)
         if (current == this.id) {
+            score++
+            document.getElementById('score').innerHTML = "score:" + score;
             rightAns++;
             winAudio.play();
         } else {
             window.navigator.vibrate(200);
         }
         noOfGuess--;
+        document.getElementById('guess').innerHTML = "Guess:" + noOfGuess;
         if (noOfGuess == 0) {
             mainCard.src = 'images/back.svg';
             if (rightAns >= pass) {
-                alert('Congratulations!');
+                alert('Your score is: ' + rightAns + "\n" + msg);
             } else {
-                alert('Try Again!');
+                alert('Your score is: ' + rightAns + "\n" + 'Sorry you did not earn ESP!');
+
             }
+            location.reload();
         }
     });
 }
