57e5895b6c16a9e2eaa010ecdf68743df0975adb
djwilliams2020 <68558050+djwilliams2020@users.noreply.github.com>
2020-10-06 11:54:10 -0600

    Update card.js

--- card.js
+++ card.js
@@ -1,4 +1,4 @@
-
+ 
 var noOfGuess = 10;
 var pass = 5;
 var guessChoices = ['star', 'circle', 'square', 'wave', 'plus'];
@@ -7,11 +7,8 @@ var winAudio = new Audio('sound/win.wav');
 var mainCard = document.getElementById('maincard');
 var choices = document.getElementsByClassName('choice');
 var msg = 'Congratulations you have earned ESP!';
-<<<<<<< HEAD
 var score = 0;
-=======
-var score =0;
->>>>>>> 497826f9e72cb2126df7729a589265787407c23d
+var score = 0;
 
 for (i = 0; i < choices.length; i++) {
     let choice = choices[i];
@@ -38,10 +35,10 @@ for (i = 0; i < choices.length; i++) {
         if (noOfGuess == 0) {
             mainCard.src = 'images/back.svg';
             if (rightAns >= pass) {
-              alert('Your score is: ' + rightAns + "\n" + msg);
+                alert('Your score is: ' + rightAns + "\n" + msg);
             } else {
                 alert('Your score is: ' + rightAns + "\n" + 'Sorry you did not earn ESP!');
- 
+
             }
             location.reload();
         }
